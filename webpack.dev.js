745375c578113a41605fdbc231d054f685cdf051
Roser Reverte <reverroser@gmail.com>
2020-04-06 20:24:14 +0200

    sass set up

--- webpack.dev.js
+++ webpack.dev.js
@@ -14,11 +14,11 @@ module.exports = {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
-            // {
-            //     test: /\.scss$/,
-            //     use: ['style-loader', 'css-loader', 'sass-loader']
-            // }
         ]
     }
     // plugins: [
