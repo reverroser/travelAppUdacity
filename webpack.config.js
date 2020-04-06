41fbf7e256c9bc69c21171a24c0785c67ace0b25
Roser Reverte <reverroser@gmail.com>
2020-04-06 20:27:40 +0200

    clean-webpack-plugin set up

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,10 @@ const webpack = require('webpack')
 const webpackDev = require('./webpack.dev');
 const webpackProd = require('./webpack.prod');
 
-// module.exports = env => {
-//     if (env.NODE_ENV === 'development') {
-//         return webpackDev;
-//     }
+module.exports = env => {
+    if (env.NODE_ENV === 'development') {
+        return webpackDev;
+    }
 
-//     return webpackProd;
-// };
+    return webpackProd;
+};
