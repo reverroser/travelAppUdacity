d227b3cc573c5f09d6d21c299e1ccfae0f198e90
Roser Reverte <reverroser@gmail.com>
2020-04-06 20:26:14 +0200

    html webpack plugin setup

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
-// const HtmlWebPackPlugin = require("html-webpack-plugin")
+const HtmlWebPackPlugin = require("html-webpack-plugin")
 // const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
@@ -21,19 +21,19 @@ module.exports = {
             }
         ]
     }
-    // plugins: [
-    //     new HtmlWebPackPlugin({
-    //         template: "./src/client/views/index.html",
-    //         filename: "./index.html",
-    //     }),
-    //     new CleanWebpackPlugin({
-    //         // Simulate the removal of files
-    //         dry: true,
-    //         // Write Logs to Console
-    //         verbose: true,
-    //         // Automatically remove all unused webpack assets on rebuild
-    //         cleanStaleWebpackAssets: true,
-    //         protectWebpackAssets: false
-    //     })
-    // ]
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+        //     new CleanWebpackPlugin({
+        //         // Simulate the removal of files
+        //         dry: true,
+        //         // Write Logs to Console
+        //         verbose: true,
+        //         // Automatically remove all unused webpack assets on rebuild
+        //         cleanStaleWebpackAssets: true,
+        //         protectWebpackAssets: false
+        //     })
+    ]
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -13,11 +13,11 @@ module.exports = {
                 loader: "babel-loader"
             }
         ]
-    }
-    // plugins: [
-    //     new HtmlWebPackPlugin({
-    //         template: "./src/client/views/index.html",
-    //         filename: "./index.html",
-    //     })
-    // ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+    ]
 }
