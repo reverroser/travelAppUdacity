08f25e5c8c5d6a6be4c9df2cbd32a3ffd85600d0
Roser Reverte <reverroser@gmail.com>
2020-04-13 13:02:23 +0200

    materialize UI

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,9 @@
-import './styles/main.scss'
+// Local js
+import './js/app.js';
+
+// Materialize css
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
+
+// Local styles
+import './styles/main.scss';
---
+++ src/client/js/app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Init Materialize css modals
+    const elems = document.querySelectorAll('.modal');
+    M.Modal.init(elems);
+});
