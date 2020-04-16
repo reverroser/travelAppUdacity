b147c5c0c3436322d8057dd0cf6982ad00ec2393
Roser Reverte <reverroser@gmail.com>
2020-04-16 19:46:26 +0200

    trip form

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Init Materialize css modals
-    const elems = document.querySelectorAll('.modal');
-    M.Modal.init(elems);
+    const modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
+
+    // Init Materialize css date pickers
+    const datePickers = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datePickers);
 });
